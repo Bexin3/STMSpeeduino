4780052dcc2cba05b2d282e3179101259bcf195b
Benjamin <88098139+Bexin3@users.noreply.github.com>
2023-12-13 14:18:22 +0000

    Update STMSpeeduino.h

--- src/STMSpeeduino.h
+++ src/STMSpeeduino.h
@@ -20,13 +20,17 @@ void ADCInterleaved(int ADCChannel, int Resolution, bool Differential, double Cl
 void ADCSimultaneous(int ADC1Channel, int ADC2Channel, int Resolution, double ClockSpeedMHZ, int SampleTime, int Samplenum);
 void ADC1Begin(int ADCChannel, int Resolution, bool Differential, double ClockSpeedMHZ, int SampleTime, int Samplenum);
 void ADC2Begin(int ADCChannel, int Resolution, bool Differential, double ClockSpeedMHZ, int SampleTime, int Samplenum);
+void ADC3Begin(int ADCChannel, int Resolution, bool Differential, double ClockSpeedMHZ, int SampleTime, int Samplenum);
 
 int CatchADC1Value(uint32_t Timeout);
 int CatchADC2Value(uint32_t Timeout);
+int CatchADC3Value(uint32_t Timeout);
 
 void SystemCLCKInit(double ClockSpeedMHZ);
+void ADC3_Start();
 void ADC2_Start();
 void ADC1_Start();
+void ADC3_Stop();
 void ADC2_Stop();
 void ADC1_Stop();
 void ADCSimultaneous_Start();
