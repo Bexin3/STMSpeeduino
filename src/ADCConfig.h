e5a4cfe016059c29dcd47542057dedca71f35699
Benjamin <88098139+Bexin3@users.noreply.github.com>
2023-12-13 13:42:06 +0000

    Update ADCConfig.h

--- src/ADCConfig.h
+++ src/ADCConfig.h
@@ -12,8 +12,8 @@ void ADCSimultaneous(int ADC1Channel = A0, int ADC2Channel = 1, int Resolution =
 void ADC1Begin(int ADCChannel = A0, int Resolution = 16, bool Differential = 0, double ClockSpeedMHZ = 40, int SampleTime = 0, int Samplenum = 0);
 void ADC2Begin(int ADCChannel = A0, int Resolution = 16, bool Differential = 0, double ClockSpeedMHZ = 40, int SampleTime = 0, int Samplenum = 0);
 
-int CatchADC1Value();
-int CatchADC2Value();
+int CatchADC1Value(uint32_t Timeout = 0xFFFFFF);
+int CatchADC2Value(uint32_t Timeout = 0xFFFFFF);
 
 void ADC1_Init(int AdcChannel, int Resolution, bool Differential, int SampleTime, int Samplenum);
 void ADC2_Init(int AdcChannel, int Resolution, bool Differential, int SampleTime, int Samplenum);
