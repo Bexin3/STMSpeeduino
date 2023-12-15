cb7f663960272fba95ff65c93a40c3db25d5fd64
Benjamin <88098139+Bexin3@users.noreply.github.com>
2023-12-15 01:05:16 +0100

    Add a draft of FastAnalogRead and Injected Reads as well as Initiated check

--- src/ADCConfig.cpp
+++ src/ADCConfig.cpp
@@ -2,7 +2,9 @@
 #include "ADCConfig.h"
 #include "PinMappings.h"
 
-
+bool ADC1Initialized = 0;
+bool ADC2Initialized = 0;
+bool ADC3Initialized = 0;
 int Prescaler = 0;
 void ADCInterleaved(int ADCChannel, int Resolution, bool Differential, double ClockSpeedMHZ) {
 
@@ -108,7 +110,7 @@ void ADC1_Init(int AdcChannel, int Resolution, bool Differential, int SampleTime
 
 
 
-  SET_BIT(ADC1->CFGR, ADC_CFGR_OVRMOD_Msk | ADC_CFGR_CONT_Msk);  //Enable Continuous mode, Always overwrite data
+  SET_BIT(ADC1->CFGR, ADC_CFGR_OVRMOD_Msk | ADC_CFGR_CONT_Msk | ADC_CFGR_JQM | ADC_CFGR_JDISCEN);  //Enable Continuous mode, Always overwrite data
 
   ResolutionSet(Resolution);  //Set resolution
 
@@ -121,7 +123,7 @@ void ADC1_Init(int AdcChannel, int Resolution, bool Differential, int SampleTime
 
   SET_BIT(ADC1->CR, ADC_CR_ADEN_Msk);                               //Enable ADC
   
-
+    ADC1Initialized = 1;
 }
 
 
@@ -175,7 +177,7 @@ void ADC2_Init(int AdcChannel, int Resolution, bool Differential, int SampleTime
 
 
 
-  SET_BIT(ADC2->CFGR, ADC_CFGR_OVRMOD_Msk | ADC_CFGR_CONT_Msk);  //Enable Continuous mode, Always overwrite data
+  SET_BIT(ADC2->CFGR, ADC_CFGR_OVRMOD_Msk | ADC_CFGR_CONT_Msk | ADC_CFGR_JQM | ADC_CFGR_JDISCEN);  //Enable Continuous mode, Always overwrite data
 
   Resolution2Set(Resolution);  //Set RESOLUTION
 
@@ -186,6 +188,8 @@ void ADC2_Init(int AdcChannel, int Resolution, bool Differential, int SampleTime
 
 
   SET_BIT(ADC2->CR, ADC_CR_ADEN_Msk);  
+    
+    ADC1Initialized = 2;
 
 }
 
@@ -240,7 +244,7 @@ void ADC3_Init(int AdcChannel, int Resolution, bool Differential, int SampleTime
 
 
 
-  SET_BIT(ADC3->CFGR, ADC_CFGR_OVRMOD_Msk | ADC_CFGR_CONT_Msk);  //Enable Continuous mode, Always overwrite data
+  SET_BIT(ADC3->CFGR, ADC_CFGR_OVRMOD_Msk | ADC_CFGR_CONT_Msk | ADC_CFGR_JQM | ADC_CFGR_JDISCEN);  //Enable Continuous mode, Always overwrite data
 
   Resolution2Set(Resolution);  //Set RESOLUTION
 
@@ -251,6 +255,8 @@ void ADC3_Init(int AdcChannel, int Resolution, bool Differential, int SampleTime
 
 
   SET_BIT(ADC3->CR, ADC_CR_ADEN_Msk);
+    
+    ADC3Initialized = 1;
 
 }
 
@@ -353,7 +359,7 @@ void ADC3_Start() {
 
 void ADC3_Stop() {
   
-  SET_BIT(ADC3->CR, ADC_CR_ADSTP);
+  SET_BIT(ADC3->CR, ADC_CR_ADSTP); //Clear result ready flag.
   
 }
 
@@ -529,3 +535,112 @@ int CatchADC3Value(uint32_t Timeout) {
     while (!READ_REG(ADC3->ISR & ADC_ISR_EOC) && (micros() < (InitMicros + Timeout))) {}; //Wait for a new value if the latest one was already read
     return(READ_REG(ADC3->DR)); //Return the new value
 }
+
+
+int FastAnalogRead(int Pin) {
+  switch (Pin) {
+          
+      case A0: if (ADC1Initialized) {return(ADC1InjectedRead(A0));}
+               else if (ADC2Initialized) {return(ADC2InjectedRead(A0));}
+               else {return(ADC1InjectedRead(A0));};
+      case A1: if (ADC1Initialized) {return(ADC1InjectedRead(A1));}
+               else if (ADC2Initialized) {return(ADC2InjectedRead(A1));}
+               else {return(ADC1InjectedRead(A1));};
+      case A2: if (ADC1Initialized) {return(ADC1InjectedRead(A2));}
+               else if (ADC2Initialized) {return(ADC2InjectedRead(A2));}
+               else {return(ADC1InjectedRead(A2));};
+      case A3: if (ADC1Initialized) {return(ADC1InjectedRead(A3));}
+               else if (ADC2Initialized) {return(ADC2InjectedRead(A3));}
+               else {return(ADC1InjectedRead(A3));};
+      case A4: if (ADC1Initialized) {return(ADC1InjectedRead(A4));}
+               else if (ADC2Initialized) {return(ADC2InjectedRead(A4));}
+               else {return(ADC1InjectedRead(A4));};
+      case A5: if (ADC1Initialized) {return(ADC1InjectedRead(A5));}
+               else if (ADC2Initialized) {return(ADC2InjectedRead(A5));}
+               else if (ADC3Initialized) {return(ADC3InjectedRead(A5));}
+               else {return(ADC1InjectedRead(A5));};
+      case A6: if (ADC1Initialized) {return(ADC1InjectedRead(A6));}
+               else if (ADC2Initialized) {return(ADC2InjectedRead(A6));}
+               else if (ADC3Initialized) {return(ADC3InjectedRead(A6));}
+               else {return(ADC1InjectedRead(A6));};
+      case A7: return(ADC1InjectedRead(A7));
+      //A8-A11
+      case 102: return(ADC3InjectedRead(102));
+      case 103: return(ADC3InjectedRead(103));
+      case 100: if (ADC1Initialized) {return(ADC1InjectedRead(100));}
+                else if (ADC2Initialized) {return(ADC2InjectedRead(100));}
+                else {return(ADC1InjectedRead(100));};
+      case 101: if (ADC1Initialized) {return(ADC1InjectedRead(101));}
+                else if (ADC2Initialized) {return(ADC2InjectedRead(101));}
+                else {return(ADC1InjectedRead(101));};
+
+      case DAC_0: if (ADC1Initialized) {return(ADC1InjectedRead(DAC_0));}
+                else if (ADC2Initialized) {return(ADC2InjectedRead(DAC_0));}
+                else {return(ADC1InjectedRead(DAC_0));};
+      case DAC_1: if (ADC1Initialized) {return(ADC1InjectedRead(DAC_1));}
+                else if (ADC2Initialized) {return(ADC2InjectedRead(DAC_1));}
+                else {return(ADC1InjectedRead(DAC_1));};
+
+      case 2: if (ADC1Initialized) {return(ADC1InjectedRead(2));}
+                else if (ADC2Initialized) {return(ADC2InjectedRead(2));}
+                else {return(ADC1InjectedRead(2));};
+      case 3: if (ADC1Initialized) {return(ADC1InjectedRead(3));}
+                else if (ADC2Initialized) {return(ADC2InjectedRead(3));}
+                else {return(ADC1InjectedRead(3));};
+      case 5: if (ADC1Initialized) {return(ADC1InjectedRead(5));}
+                else if (ADC2Initialized) {return(ADC2InjectedRead(5));}
+                else {return(ADC1InjectedRead(5));};
+      case 21: return(ADC3InjectedRead(21));
+      case 56: if (ADC1Initialized) {return(ADC1InjectedRead(56));}
+                else if (ADC2Initialized) {return(ADC2InjectedRead(56));}
+                else {return(ADC1InjectedRead(56));};
+      case 66: return(ADC1InjectedRead(66));
+      case 73: if (ADC1Initialized) {return(ADC1InjectedRead(73));}
+               else if (ADC2Initialized) {return(ADC2InjectedRead(73));}
+               else if (ADC3Initialized) {return(ADC3InjectedRead(73));}
+               else {return(ADC1InjectedRead(73));};
+               
+      default: return(-1);
+  };
+}
+
+int ADC1InjectedRead(int ADCChannel) {
+    if (!ADC1Initialized) {
+        ADC1Begin();
+    };
+    while (!READ_REG(ADC1->ISR & ADC_ISR_ADRDY)) {};
+    ADC1->JSQR = 512 * ADC1PinRemap(ADCChannel);
+    while (!READ_REG(ADC2->ISR & ADC_ISR_ADRDY)) {};
+    uint32_t InitMicros = micros();
+    SET_BIT(ADC1->CR, ADC_CR_JADSTART);
+    while (!READ_REG(ADC1->ISR & ADC_ISR_JEOC) && (micros() < (0xFFF + InitMicros))) {}; //Wait for a new value if the latest one was already read
+    return(READ_REG(ADC1->JDR1)); //Return the new value
+}
+
+int ADC2InjectedRead(int ADCChannel) {
+    if (!ADC2Initialized) {
+        ADC2Begin();
+    };
+    while (!READ_REG(ADC2->ISR & ADC_ISR_ADRDY)) {};
+    ADC2->JSQR = 512 * ADC2PinRemap(ADCChannel);
+    while (!READ_REG(ADC2->ISR & ADC_ISR_ADRDY)) {};
+    uint32_t InitMicros = micros();
+    SET_BIT(ADC2->CR, ADC_CR_JADSTART);
+    while (!READ_REG(ADC2->ISR & ADC_ISR_JEOC) && (micros() < (0xFFF + InitMicros))) {}; //Wait for a new value if the latest one was already read
+    return(READ_REG(ADC2->JDR1)); //Return the new value
+}
+
+int ADC3InjectedRead(int ADCChannel) {
+    if (!ADC3Initialized) {
+        ADC3Begin();
+    };
+    while (!READ_REG(ADC3->ISR & ADC_ISR_ADRDY)) {};
+    ADC3->JSQR = 512 * ADC3PinRemap(ADCChannel);
+    while (!READ_REG(ADC3->ISR & ADC_ISR_ADRDY)) {};
+    uint32_t InitMicros = micros();
+    SET_BIT(ADC3->CR, ADC_CR_JADSTART);
+    while (!READ_REG(ADC3->ISR & ADC_ISR_JEOC) && (micros() < (0xFFF + InitMicros))) {}; //Wait for a new value if the latest one was already read
+    return(READ_REG(ADC3->JDR1)); //Return the new value
+}
+
+//Add change channel
