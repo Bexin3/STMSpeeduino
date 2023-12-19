8b18ee4a8493a8d10ae3a7e3fa55d8156115c805
Benjamin <88098139+Bexin3@users.noreply.github.com>
2023-12-19 22:41:48 +0100

    Update DmaSimultaneous.ino

--- examples/DmaSimultaneous/DmaSimultaneous.ino
+++ examples/DmaSimultaneous/DmaSimultaneous.ino
@@ -1,7 +1,7 @@
 #include "STMSpeeduino.h"
 #include "SDRAM.h" //So values can be stored in SDRAM
 
-int ValuesToBuffer = 100
+const uint16_t ValuesToBuffer = 100;
 uint16_t *FrameBuffer1 = (uint16_t *) 0x60000000;
 uint16_t *FrameBuffer2 = (uint16_t *) 0x600F0000; 
 
@@ -9,6 +9,7 @@ uint16_t *FrameBuffer2 = (uint16_t *) 0x600F0000;
 
 void setup() {
 
+SDRAM.begin();
 ADCSimultaneous(); //Begin ADCs in simultaneous mode
 
 captureSimultaneousValues(ValuesToBuffer, FrameBuffer1, FrameBuffer2);
@@ -21,11 +22,11 @@ for (int i = 0; i < ValuesToBuffer; i++) {
 Serial.print("ADC1 value ");
 Serial.print(i);
 Serial.print(": ");
-Serial.println(_fb[i]);
+Serial.println(FrameBuffer1[i]);
 Serial.print("ADC2 value ");
 Serial.print(i);
 Serial.print(": ");
-Serial.println(_fb[i]);
+Serial.println(FrameBuffer2[i]);
 
 };
 
@@ -33,4 +34,3 @@ recaptureSimultaneousValues(); //Start the capture again
 delay(100); //Wait 0.1s
 
 }
-
