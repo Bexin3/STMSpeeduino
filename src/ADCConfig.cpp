37c46905008b2157e5b7af0fc1fd8627abcda470
Benjamin <88098139+Bexin3@users.noreply.github.com>
2023-12-19 21:26:55 +0100

    Add descriptions

--- src/ADCConfig.cpp
+++ src/ADCConfig.cpp
@@ -2,68 +2,68 @@
 #include "ADCConfig.h"
 #include "PinMappings.h"
 
-bool ADC1Initialized = 0;
-bool ADC2Initialized = 0;
-bool ADC3Initialized = 0;
-int Prescaler = 0;
-bool ClockInitialized = 0;
+bool ADC1Initialized = 0; //Stores information on if ADC1 is Initialized
+bool ADC2Initialized = 0; //Stores information on if ADC2 is Initialized
+bool ADC3Initialized = 0; //Stores information on if ADC3 is Initialized
+int Prescaler = 0; //Stores the Calculated Prescaler value
+bool ClockInitialized = 0; //Stores if clock is initialized
 
 void ADCInterleaved(int ADCChannel, int Resolution, bool Differential, double ClockSpeedMHZ) {
 
-if (!ClockInitialized) {SystemCLCKInit(ClockSpeedMHZ);};
-ADC1_Init(ADCChannel, Resolution, Differential, 0, 0);
-ADC2_Init(ADCChannel, Resolution, Differential, 0, 0);
-ADCInterleavedConfig(Resolution);
-    ADC1EN();
-    ADC2EN();
-    while (!READ_REG(ADC2->ISR & ADC_ISR_ADRDY)) {};
-    while (!READ_REG(ADC1->ISR & ADC_ISR_ADRDY)) {};
-    ADC1_Start();
+if (!ClockInitialized) {SystemCLCKInit(ClockSpeedMHZ);}; //Initiate System clock
+ADC1_Init(ADCChannel, Resolution, Differential, 0, 0); //Initiate ADC1
+ADC2_Init(ADCChannel, Resolution, Differential, 0, 0); //Initiate ADC2
+ADCInterleavedConfig(Resolution); //Configure Interleaved mode
+    ADC1EN(); //Enable ADC1
+    ADC2EN(); //Enable ADC2
+    while (!READ_REG(ADC2->ISR & ADC_ISR_ADRDY)) {}; //Waits for ADC1 to be ready
+    while (!READ_REG(ADC1->ISR & ADC_ISR_ADRDY)) {}; //Waits for ADC2 to be ready
+    ADC1_Start(); //Starts the conversions
 
-}
+} //Initiates and starts ADC1 and ADC2 in Interleaved modes.
 
 void ADCSimultaneous(int ADC1Channel, int ADC2Channel, int Resolution, double ClockSpeedMHZ, int SampleTime, int Samplenum) {
 
-if (!ClockInitialized) {SystemCLCKInit(ClockSpeedMHZ);};
-SystemCLCKInit(ClockSpeedMHZ);
-ADC1_Init(ADC1Channel, Resolution, 0, SampleTime, Samplenum);
-ADC2_Init(ADC2Channel, Resolution, 0, SampleTime, Samplenum);
-ADCSimultaneousConfig();
-    ADC1EN();
-    ADC2EN();
-    while (!READ_REG(ADC2->ISR & ADC_ISR_ADRDY)) {};
-    while (!READ_REG(ADC1->ISR & ADC_ISR_ADRDY)) {};
-    ADC1_Start();
+if (!ClockInitialized) {SystemCLCKInit(ClockSpeedMHZ);}; //Initiate System clock
+ADC1_Init(ADC1Channel, Resolution, 0, SampleTime, Samplenum); //Initiate ADC1
+ADC2_Init(ADC2Channel, Resolution, 0, SampleTime, Samplenum); //Initiate ADC2
+ADCSimultaneousConfig(); //Configure Simultaneous mode
+    ADC1EN(); //Enable ADC1
+    ADC2EN(); //Enable ADC2
+    while (!READ_REG(ADC2->ISR & ADC_ISR_ADRDY)) {}; //Waits for ADC1 to be ready
+    while (!READ_REG(ADC1->ISR & ADC_ISR_ADRDY)) {}; //Waits for ADC2 to be ready
+    ADC1_Start(); //Starts the conversions
 
-}
+} //Initiates and starts ADC1 and ADC2 in Simultaneous modes.
+
 
 void ADC1Begin(int ADCChannel, int Resolution, bool Differential, double ClockSpeedMHZ, int SampleTime, int Samplenum) {
 
-if (!ClockInitialized) {SystemCLCKInit(ClockSpeedMHZ);};
-ADC1_Init(ADCChannel, Resolution, Differential, SampleTime, Samplenum);
-ADC1EN();
-ADC1_Start();
+if (!ClockInitialized) {SystemCLCKInit(ClockSpeedMHZ);}; //Initiate System clock
+ADC1_Init(ADCChannel, Resolution, Differential, SampleTime, Samplenum); //Initiate ADC1
+ADC1EN(); //Enable ADC1
+ADC1_Start(); //Start ADC1 conversions
     
 
-}
+} //Initiates and starts ADC1
 
 void ADC2Begin(int ADCChannel, int Resolution, bool Differential, double ClockSpeedMHZ, int SampleTime, int Samplenum) {
 
-if (!ClockInitialized) {SystemCLCKInit(ClockSpeedMHZ);};
-ADC2_Init(ADCChannel, Resolution, Differential, SampleTime, Samplenum);
-ADC2EN();
-ADC2_Start();
+if (!ClockInitialized) {SystemCLCKInit(ClockSpeedMHZ);}; //Initiate System clock
+ADC2_Init(ADCChannel, Resolution, Differential, SampleTime, Samplenum); //Initiate ADC2
+ADC2EN(); //Enable ADC2
+ADC2_Start(); //Start ADC2 conversions
 
-}
+} //Initiates and starts ADC2
 
 void ADC3Begin(int ADCChannel, int Resolution, bool Differential, double ClockSpeedMHZ, int SampleTime, int Samplenum) {
 
-if (!ClockInitialized) {SystemCLCKInit(ClockSpeedMHZ);};
-ADC3_Init(ADCChannel, Resolution, Differential, SampleTime, Samplenum);
-ADC3EN();
-ADC3_Start();
+if (!ClockInitialized) {SystemCLCKInit(ClockSpeedMHZ);}; //Initiate System clock
+ADC3_Init(ADCChannel, Resolution, Differential, SampleTime, Samplenum); //Initiate ADC3
+ADC3EN(); //Enable ADC3
+ADC3_Start(); //Start ADC3 conversions
 
-}
+} //Initiates and starts ADC3
 
 
 
@@ -85,10 +85,10 @@ void ADC1_Init(int AdcChannel, int Resolution, bool Differential, int SampleTime
 
   AdcChannel=ADC1PinRemap(AdcChannel);     //Get actual channel number
 
-  //ADC CALIBRATION-----------------------------------
+  //ADC CALIBRATION
 
   if(Differential) {
-  SET_BIT(ADC1->CR, ADC_CR_ADCALDIF);
+  SET_BIT(ADC1->CR, ADC_CR_ADCALDIF); 
   };
   SET_BIT(ADC1->CR, ADC_CR_ADCALLIN);
   SET_BIT(ADC1->CR, ADC_CR_ADCAL);
@@ -101,19 +101,15 @@ void ADC1_Init(int AdcChannel, int Resolution, bool Differential, int SampleTime
   ADC1->CFGR2 += 65536 * Samplenum; //Set number of samples
 
 
-  //Select channel
 
 
-  ADC1->PCSEL = 0xFFFFF;
+  ADC1->PCSEL = 0xFFFFF; //Enable all channels
 
-  ADC1->SQR1 = 64 * AdcChannel;
-
-
+  ADC1->SQR1 = 64 * AdcChannel;   //Select channel
 
 
 
-
-  ADC1->DIFSEL = 1048575 * Differential;  //Diffmode
+  ADC1->DIFSEL = 1048575 * Differential;  //Enable differential mode if needed
 
 
 
@@ -125,14 +121,8 @@ void ADC1_Init(int AdcChannel, int Resolution, bool Differential, int SampleTime
 
   ADC1->SMPR1 = SampleTime;  //Set sampletime
 
-
-
-
-  //SET_BIT(ADC1->CR, ADC_CR_ADEN_Msk);                               //Enable ADC
-    
-   // ADC12_COMMON->CCR += 262144*Prescaler; //
   
-    ADC1Initialized = 1;
+  ADC1Initialized = 1;
 }
 
 
@@ -148,11 +138,11 @@ void ADC2_Init(int AdcChannel, int Resolution, bool Differential, int SampleTime
   CLEAR_BIT(ADC2->CR, ADC_CR_DEEPPWD);
 
   SET_BIT(ADC2->CR, ADC_CR_ADVREGEN_Msk);  //Enable the voltage generator
+
+
+  AdcChannel=ADC2PinRemap(AdcChannel); //Get actual channel number
+
   //ADC CALIBRATION-----------------------------------
-
-  AdcChannel=ADC2PinRemap(AdcChannel);
-
-
   if(Differential) {
   SET_BIT(ADC2->CR, ADC_CR_ADCALDIF);
   };
@@ -160,23 +150,20 @@ void ADC2_Init(int AdcChannel, int Resolution, bool Differential, int SampleTime
   SET_BIT(ADC2->CR, ADC_CR_ADCAL);
   while (READ_REG(ADC2->CR & ADC_CR_ADCAL)) {};
 
-  SET_BIT(ADC2->CFGR2, ADC_CFGR2_ROVSE);
+  SET_BIT(ADC2->CFGR2, ADC_CFGR2_ROVSE); //Allow Oversampling
 
-  ADC2->CFGR2 += 65536 * Samplenum;
-
-
+  ADC2->CFGR2 += 65536 * Samplenum; //Set number of samples
 
 
- 
 
-  ADC2->PCSEL = 0xFFFFF;
+  ADC2->PCSEL = 0xFFFFF; //Enable all channels
 
-  ADC2->SQR1 = 64 * AdcChannel;
+  ADC2->SQR1 = 64 * AdcChannel; //Select a channel
 
 
 
 
-  ADC2->DIFSEL = 1048575 * Differential;  //Diffmode?
+  ADC2->DIFSEL = 1048575 * Differential;  //Enable differential mode if needed
 
 
 
@@ -212,11 +199,11 @@ void ADC3_Init(int AdcChannel, int Resolution, bool Differential, int SampleTime
   CLEAR_BIT(ADC3->CR, ADC_CR_DEEPPWD);
 
   SET_BIT(ADC3->CR, ADC_CR_ADVREGEN_Msk);  //Enable the voltage generator
+
+
+  AdcChannel=ADC3PinRemap(AdcChannel); //Get actual channel id
+
   //ADC CALIBRATION-----------------------------------
-
-  AdcChannel=ADC3PinRemap(AdcChannel);
-
-
   if(Differential) {
   SET_BIT(ADC3->CR, ADC_CR_ADCALDIF);
   };
@@ -224,21 +211,21 @@ void ADC3_Init(int AdcChannel, int Resolution, bool Differential, int SampleTime
   SET_BIT(ADC3->CR, ADC_CR_ADCAL);
   while (READ_REG(ADC3->CR & ADC_CR_ADCAL)) {};
 
-  SET_BIT(ADC3->CFGR2, ADC_CFGR2_ROVSE);
+  SET_BIT(ADC3->CFGR2, ADC_CFGR2_ROVSE); //Allow oversampling
 
-  ADC3->CFGR2 += 65536 * Samplenum;
-
-
+  ADC3->CFGR2 += 65536 * Samplenum; //Set number of samples
 
 
-  ADC3->PCSEL = 0xFFFFF;
 
-  ADC3->SQR1 = 64 * AdcChannel;
+
+  ADC3->PCSEL = 0xFFFFF; //Enable all ADC Channels
+
+  ADC3->SQR1 = 64 * AdcChannel; //Select ADC Channel
  
 
 
 
-  ADC3->DIFSEL = 1048575 * Differential;  //Diffmode?
+  ADC3->DIFSEL = 1048575 * Differential;  //Enable differential mode if needed
 
 
 
@@ -263,75 +250,41 @@ void ADC3_Init(int AdcChannel, int Resolution, bool Differential, int SampleTime
 
 
 
-void Resolution2Set(int Resolution) {
-
-  //Sets Resolution
-
-  CLEAR_BIT(ADC2->CFGR, ADC_CFGR_RES);
-
-  switch (Resolution) {
-    case 8:
-      SET_BIT(ADC2->CFGR, ADC_CFGR_RES);
-      break;
-    case 10:
-      SET_BIT(ADC2->CFGR, ADC_CFGR_RES_0 | ADC_CFGR_RES_1);
-      break;
-    case 12:
-      SET_BIT(ADC2->CFGR, ADC_CFGR_RES_2 | ADC_CFGR_RES_1);
-      break;
-    case 14:
-      SET_BIT(ADC2->CFGR, ADC_CFGR_RES_0 | ADC_CFGR_RES_2);
-      break;
-    default:
-      break;
-  };
-}
-
-
 
 void ADCInterleavedConfig(int Resolution) {
 
 
-    SET_BIT(ADC12_COMMON->CCR, ADC_CCR_DUAL_0 | ADC_CCR_DUAL_1);
-/*
-if (Resolution = 16) {
-   ADC12_COMMON->CCR = 7+(2*256);
-} else {
-   ADC12_COMMON->CCR = 7+(1*256);
-};
-*/
-
-
+    SET_BIT(ADC12_COMMON->CCR, ADC_CCR_DUAL_0 | ADC_CCR_DUAL_1); //Configures Interleaved mode
 
 }
 
 
 void ADCSimultaneousConfig() {
     
-    SET_BIT(ADC12_COMMON->CCR, ADC_CCR_DUAL_0);
+    SET_BIT(ADC12_COMMON->CCR, ADC_CCR_DUAL_0); //Configure Simultaneous mode
     
 }
     
 void ADC1EN() {
-    ADC12_COMMON->CCR |= Prescaler << 18;
-        SET_BIT(ADC1->CR, ADC_CR_ADEN_Msk);
+    ADC12_COMMON->CCR |= Prescaler << 18; //Set Prescaler value
+        SET_BIT(ADC1->CR, ADC_CR_ADEN_Msk); //Enable
 }
 
 void ADC2EN() {
-    ADC12_COMMON->CCR |= Prescaler << 18;
-        SET_BIT(ADC2->CR, ADC_CR_ADEN_Msk);
+    ADC12_COMMON->CCR |= Prescaler << 18; //Set Prescaler value
+        SET_BIT(ADC2->CR, ADC_CR_ADEN_Msk); //Enable
 }
 
 void ADC3EN() {
-    ADC3_COMMON->CCR |= Prescaler << 18;
-        SET_BIT(ADC3->CR, ADC_CR_ADEN_Msk);
+    ADC3_COMMON->CCR |= Prescaler << 18; //Set Prescaler value
+        SET_BIT(ADC3->CR, ADC_CR_ADEN_Msk); //Enable
 }
 
 void ADC1_Start() {
 
 
 
-  while (!READ_REG(ADC1->ISR & ADC_ISR_ADRDY)) {};
+  while (!READ_REG(ADC1->ISR & ADC_ISR_ADRDY)) {}; //Wait for ADC to be ready to start
 
   SET_BIT(ADC1->CR, ADC_CR_ADSTART_Msk);  //Start
 
@@ -342,7 +295,7 @@ void ADC2_Start() {
 
 
 
-  while (!READ_REG(ADC2->ISR & ADC_ISR_ADRDY)) {};
+  while (!READ_REG(ADC2->ISR & ADC_ISR_ADRDY)) {}; //Wait for ADC to be ready to start
 
   SET_BIT(ADC2->CR, ADC_CR_ADSTART_Msk);  //Start
 
@@ -352,7 +305,7 @@ void ADC3_Start() {
 
 
 
-  while (!READ_REG(ADC3->ISR & ADC_ISR_ADRDY)) {};
+  while (!READ_REG(ADC3->ISR & ADC_ISR_ADRDY)) {}; //Wait for ADC to be ready to start
 
   SET_BIT(ADC3->CR, ADC_CR_ADSTART_Msk);  //Start
 
@@ -361,60 +314,60 @@ void ADC3_Start() {
 
 void ADC3_Stop() {
   
-  SET_BIT(ADC3->CR, ADC_CR_ADSTP); //Clear result ready flag.
-  while (READ_REG(ADC3->CR & ADC_CR_ADSTP)) {};
+  SET_BIT(ADC3->CR, ADC_CR_ADSTP); //Stop the ADC
+  while (READ_REG(ADC3->CR & ADC_CR_ADSTP)) {}; //Wait for ADC to stop
 
 }
 
 void ADC2_Stop() {
   
-  SET_BIT(ADC2->CR, ADC_CR_ADSTP);
-  while (READ_REG(ADC2->CR & ADC_CR_ADSTP)) {};
+  SET_BIT(ADC2->CR, ADC_CR_ADSTP); //Stop the ADC
+  while (READ_REG(ADC2->CR & ADC_CR_ADSTP)) {}; //Wait for ADC to stop
   
 }
 
 void ADC1_Stop() {
   
-  SET_BIT(ADC1->CR, ADC_CR_ADSTP);
-  while (READ_REG(ADC1->CR & ADC_CR_ADSTP)) {};
+  SET_BIT(ADC1->CR, ADC_CR_ADSTP); //Stop the ADC
+  while (READ_REG(ADC1->CR & ADC_CR_ADSTP)) {}; //Wait for ADC to stop
 
 }
 
 
 void ADC1ChangeChannel(int AdcChannel) {
-    bool ADActive = READ_REG(ADC1->CR & ADC_CR_ADSTART);
-    if(ADActive) {ADC1_Stop();};
+    bool ADActive = READ_REG(ADC1->CR & ADC_CR_ADSTART); //Check if ADC is active
+    if(ADActive) {ADC1_Stop();}; //Stop the ADC if active
     
-    while (READ_REG(ADC1->CR & ADC_CR_ADSTP) || READ_REG(ADC1->CR & ADC_CR_JADSTART)) {};
-    ADC1->SQR1 = 64 * ADC1PinRemap(AdcChannel);
+    while (READ_REG(ADC1->CR & ADC_CR_ADSTP) || READ_REG(ADC1->CR & ADC_CR_JADSTART)) {}; //Wait for ADC to stop
+    ADC1->SQR1 = 64 * ADC1PinRemap(AdcChannel); //Set ADC Channel
     
-    if(ADActive) {ADC1_Start();};
+    if(ADActive) {ADC1_Start();}; //If ADC was initially active turn it back on
 }
 
 void ADC2ChangeChannel(int AdcChannel) {
-    bool ADActive = READ_REG(ADC2->CR & ADC_CR_ADSTART);
-    if(ADActive) {ADC2_Stop();};
+    bool ADActive = READ_REG(ADC2->CR & ADC_CR_ADSTART); //Check if ADC is active
+    if(ADActive) {ADC2_Stop();}; //Stop the ADC if active
     
-    while (READ_REG(ADC2->CR & ADC_CR_ADSTP) || READ_REG(ADC2->CR & ADC_CR_JADSTART)) {};
-    ADC2->SQR1 = 64 * ADC2PinRemap(AdcChannel);
+    while (READ_REG(ADC2->CR & ADC_CR_ADSTP) || READ_REG(ADC2->CR & ADC_CR_JADSTART)) {}; //Wait for ADC to stop
+    ADC2->SQR1 = 64 * ADC2PinRemap(AdcChannel); //Set ADC Channel
     
-    if(ADActive) {ADC2_Start();};
+    if(ADActive) {ADC2_Start();}; //If ADC was initially active turn it back on
 }
 
 void ADC3ChangeChannel(int AdcChannel) {
-    bool ADActive = READ_REG(ADC3->CR & ADC_CR_ADSTART);
-    if(ADActive) {ADC3_Stop();};
+    bool ADActive = READ_REG(ADC3->CR & ADC_CR_ADSTART); //Check if ADC is active
+    if(ADActive) {ADC3_Stop();};  //Stop the ADC if active
     
-    while (READ_REG(ADC3->CR & ADC_CR_ADSTP) || READ_REG(ADC3->CR & ADC_CR_JADSTART)) {};
-    ADC3->SQR1 = 64 * ADC3PinRemap(AdcChannel);
+    while (READ_REG(ADC3->CR & ADC_CR_ADSTP) || READ_REG(ADC3->CR & ADC_CR_JADSTART)) {}; //Wait for ADC to stop
+    ADC3->SQR1 = 64 * ADC3PinRemap(AdcChannel); //Set ADC Channel
     
-    if(ADActive) {ADC3_Start();};
+    if(ADActive) {ADC3_Start();}; //If ADC was initially active turn it back on
 }
 
 
 void ResolutionSet(int Resolution) {
 
-  //Sets Resolution
+  //Sets Resolution for the ADC
 
   CLEAR_BIT(ADC1->CFGR, ADC_CFGR_RES);
 
@@ -437,9 +390,35 @@ void ResolutionSet(int Resolution) {
 }
 
 
+
+void Resolution2Set(int Resolution) {
+
+  //Sets Resolution for the ADC
+
+  CLEAR_BIT(ADC2->CFGR, ADC_CFGR_RES);
+
+  switch (Resolution) {
+    case 8:
+      SET_BIT(ADC2->CFGR, ADC_CFGR_RES);
+      break;
+    case 10:
+      SET_BIT(ADC2->CFGR, ADC_CFGR_RES_0 | ADC_CFGR_RES_1);
+      break;
+    case 12:
+      SET_BIT(ADC2->CFGR, ADC_CFGR_RES_2 | ADC_CFGR_RES_1);
+      break;
+    case 14:
+      SET_BIT(ADC2->CFGR, ADC_CFGR_RES_0 | ADC_CFGR_RES_2);
+      break;
+    default:
+      break;
+  };
+}
+
+
 void Resolution3Set(int Resolution) {
 
-  //Sets Resolution
+  //Sets Resolution for the ADC
 
   CLEAR_BIT(ADC3->CFGR, ADC_CFGR_RES);
 
@@ -523,14 +502,14 @@ dividor = 128;
   //Set generator speed, remain in 150-300mhz range as long as possible
   
   RCC->PLL2DIVR = 0x01010000;  //Sets the generator speed
-  RCC->PLL2DIVR += dividor*512-512;
+  RCC->PLL2DIVR += dividor*512-512; //Calculation
   RCC->PLL2DIVR += ceil(ClockSpeedMHZ*dividor*AdcPrescDivision-4);  //Sets the generator speed
 
   SET_BIT(RCC->CR, RCC_CR_PLL2ON); //Enable generator
   SET_BIT(RCC->PLLCFGR, RCC_PLLCFGR_DIVP2EN);  //Enable output
 
 
-  SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_ADC12EN_Msk | RCC_AHB1ENR_DMA1EN);  //Enable ADC
+  SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_ADC12EN_Msk | RCC_AHB1ENR_DMA1EN);  //Enable ADC 1 and 2 and DMA1
   SET_BIT(RCC->AHB4ENR, RCC_AHB4ENR_GPIOAEN | RCC_AHB4ENR_GPIOBEN | RCC_AHB4ENR_GPIOCEN | RCC_AHB4ENR_ADC3EN); //Enable GPIOs and ADC3
 
  
@@ -550,7 +529,7 @@ if (ClockSpeedMHZ>50) {
   SET_BIT(ADC3->CR, ADC_CR_BOOST_0);
 }
 
- // delay(100); //Make sure changes got applied
+
 }
 
 
@@ -558,19 +537,19 @@ if (ClockSpeedMHZ>50) {
 
 
 int CatchADC1Value(uint32_t Timeout) {
-    uint32_t InitMicros = micros();
+    uint32_t InitMicros = micros(); //Capture current time
     while (!READ_REG(ADC1->ISR & ADC_ISR_EOC) && (micros() < (InitMicros + Timeout))) {}; //Wait for a new value if the latest one was already read
     return(READ_REG(ADC1->DR)); //Return the new value
 }
 
 int CatchADC2Value(uint32_t Timeout) {
-    uint32_t InitMicros = micros();
+    uint32_t InitMicros = micros(); //Capture current time
     while (!READ_REG(ADC2->ISR & ADC_ISR_EOC) && (micros() < (InitMicros + Timeout))) {}; //Wait for a new value if the latest one was already read
     return(READ_REG(ADC2->DR)); //Return the new value
 }
 
 int CatchADC3Value(uint32_t Timeout) {
-    uint32_t InitMicros = micros();
+    uint32_t InitMicros = micros(); //Capture current time
     while (!READ_REG(ADC3->ISR & ADC_ISR_EOC) && (micros() < (InitMicros + Timeout))) {}; //Wait for a new value if the latest one was already read
     return(READ_REG(ADC3->DR)); //Return the new value
 }
@@ -641,17 +620,17 @@ int FastAnalogRead(int Pin) {
                
       default: return(-1);
   };
-}
+} //Quickly read a pin from the first initiated ADC using injected conversion - not stopping usual conversion, or Initiate first in order ADC and read.
 
 int ADC1InjectedRead(int ADCChannel) {
-    if (!ADC1Initialized) {
+    if (!ADC1Initialized) { 
         ADC1Begin();
-    };
-    while (!READ_REG(ADC1->ISR & ADC_ISR_ADRDY)) {};
-    ADC1->JSQR = 512 * ADC1PinRemap(ADCChannel);
-    while (!READ_REG(ADC1->ISR & ADC_ISR_ADRDY)) {};
-    uint32_t InitMicros = micros();
-    SET_BIT(ADC1->CR, ADC_CR_JADSTART);
+    }; //Begin ADC if not initialized
+    while (!READ_REG(ADC1->ISR & ADC_ISR_ADRDY)) {}; //Wait for ADC to be ready
+    ADC1->JSQR = 512 * ADC1PinRemap(ADCChannel); //Set the injected pin to ID
+    while (!READ_REG(ADC1->ISR & ADC_ISR_ADRDY)) {}; //Wait for ADC to be ready
+    uint32_t InitMicros = micros(); //Capture current time
+    SET_BIT(ADC1->CR, ADC_CR_JADSTART); //Start the conversion
     while (!READ_REG(ADC1->ISR & ADC_ISR_JEOC) && (micros() < (0xFFF + InitMicros))) {}; //Wait for a new value if the latest one was already read
     return(READ_REG(ADC1->JDR1)); //Return the new value
 }
@@ -659,12 +638,12 @@ int ADC1InjectedRead(int ADCChannel) {
 int ADC2InjectedRead(int ADCChannel) {
     if (!ADC2Initialized) {
         ADC2Begin();
-    };
-    while (!READ_REG(ADC2->ISR & ADC_ISR_ADRDY)) {};
-    ADC2->JSQR = 512 * ADC2PinRemap(ADCChannel);
-    while (!READ_REG(ADC2->ISR & ADC_ISR_ADRDY)) {};
-    uint32_t InitMicros = micros();
-    SET_BIT(ADC2->CR, ADC_CR_JADSTART);
+    }; //Begin ADC if not initialized
+    while (!READ_REG(ADC2->ISR & ADC_ISR_ADRDY)) {}; //Wait for ADC to be ready
+    ADC2->JSQR = 512 * ADC2PinRemap(ADCChannel); //Set the injected pin to ID
+    while (!READ_REG(ADC2->ISR & ADC_ISR_ADRDY)) {}; //Wait for ADC to be ready
+    uint32_t InitMicros = micros(); //Capture current time
+    SET_BIT(ADC2->CR, ADC_CR_JADSTART); //Start the conversion
     while (!READ_REG(ADC2->ISR & ADC_ISR_JEOC) && (micros() < (0xFFF + InitMicros))) {}; //Wait for a new value if the latest one was already read
     return(READ_REG(ADC2->JDR1)); //Return the new value
 }
@@ -672,12 +651,12 @@ int ADC2InjectedRead(int ADCChannel) {
 int ADC3InjectedRead(int ADCChannel) {
     if (!ADC3Initialized) {
         ADC3Begin();
-    };
-    while (!READ_REG(ADC3->ISR & ADC_ISR_ADRDY)) {};
-    ADC3->JSQR = 512 * ADC3PinRemap(ADCChannel);
-    while (!READ_REG(ADC3->ISR & ADC_ISR_ADRDY)) {};
-    uint32_t InitMicros = micros();
-    SET_BIT(ADC3->CR, ADC_CR_JADSTART);
+    }; //Begin ADC if not initialized
+    while (!READ_REG(ADC3->ISR & ADC_ISR_ADRDY)) {}; //Wait for ADC to be ready
+    ADC3->JSQR = 512 * ADC3PinRemap(ADCChannel); //Set the injected pin to ID
+    while (!READ_REG(ADC3->ISR & ADC_ISR_ADRDY)) {}; //Wait for ADC to be ready
+    uint32_t InitMicros = micros(); //Capture current time
+    SET_BIT(ADC3->CR, ADC_CR_JADSTART); //Start the conversion
     while (!READ_REG(ADC3->ISR & ADC_ISR_JEOC) && (micros() < (0xFFF + InitMicros))) {}; //Wait for a new value if the latest one was already read
     return(READ_REG(ADC3->JDR1)); //Return the new value
 }
